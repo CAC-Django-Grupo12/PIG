f19398947eb541f8b573c64bda2afff2210d7888
tresPuntosG <pablociorciari@gmail.com>
2022-10-12 13:06:17 -0300

    agrego 3rutas, herencia, filtro, estaticos

---
+++ productos/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path 
+from .import views
+
+urlpatterns = [
+    path('inicio/', views.inicio, name="inicio"),
+    path('producto/<int:id>', views.producto, name="producto"),
+    path('about/', views.about),
+]
--- productos/views.py
+++ productos/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def inicio(request):
+    return render(request, "index.html")
+
+def producto(request, id):
+    return render(request, "producto.html", {"id": id, "nombre": "primer producto"})
+
+def about(request):
+    return HttpResponse("Acerca de")
