3bc1295fd3bde92075010283e8f28506de3b0fd4
coria79 <coria79@gmail.com>
2022-10-24 15:51:39 -0300

    Primera version pagina contacto

--- productos/urls.py
+++ productos/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('inicio/', views.inicio, name="inicio"),
     path('producto/<int:id>', views.producto, name="producto"),
     path('about/', views.about),
+    path('contacto/', views.contacto, name="contacto"),
 ]
--- productos/views.py
+++ productos/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .forms import ContactoForm
 
 # Create your views here.
 def inicio(request):
@@ -10,3 +11,16 @@ def producto(request, id):
 
 def about(request):
     return HttpResponse("Acerca de")
+
+def contacto(request):
+    #return HttpResponse("Pagina De Contacto")
+    if request.method == 'POST':
+        contacto_form = ContactoForm(request.POST)
+        #Si el método es POST quiere decir que el formulario ya se encuentra lleno
+        #Que será enviado al servidor
+    else:
+        #Si el método "NO" es POST quiere decir que el formulario se encuentra vacío
+        #Se renderiza un formulario nuevo vacío
+        contacto_form = ContactoForm()
+        #se crea una nueva instancia de ContactoForm y se lo asigna a la variabale contacto_form
+    return render(request, "contacto.html", {'contacto_form': contacto_form})
