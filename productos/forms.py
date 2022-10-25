d28ee405bdd1e91a9f41ae00c76c14a0dc8656cf
coria79 <coria79@gmail.com>
2022-10-25 17:46:08 -0300

    Modificacion pagina contacto

--- productos/forms.py
+++ productos/forms.py
@@ -4,4 +4,4 @@ class ContactoForm(forms.Form):
     nombre = forms.CharField(label="Nombre:")
     apellido = forms.CharField(label="Apellido:")
     correo = forms.EmailField(label="Correo:")
-    mensaje = forms.CharField(label="Apellido:")
+    mensaje = forms.CharField(label="Mesaje:")
