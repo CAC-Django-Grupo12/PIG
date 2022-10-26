c0a2a00207df45722592e308eefa5ca5cb71a1b0
tresPuntosG <pablociorciari@gmail.com>
2022-10-26 15:22:16 -0300

    año inicial

--- productos/formularios.py
+++ productos/formularios.py
@@ -12,21 +12,11 @@ class Vehiculos(forms.Form):
         (6,'4 puertas+portón'),
     )
 
-    ANIOS = (
-        ('','-Seleccione-'),
-        (2017,'2017'),
-        (2018,'2018'),
-        (2019,'2019'),
-        (2020,'2020'),
-        (2021,'2021'),
-        (2022,'2022'),
-    )
-
 
     # id=                                       # autom
     marca= forms.CharField(max_length=20, error_messages={'required': 'Por favor completa el campo',})       # tabla
     modelo= forms.CharField(max_length=30)      # tabla
-    anio = forms.IntegerField(label='Año', min_value=2017, max_value=2022, help_text='admitimos vehículos de hasta 5 años de antigüedad') 
+    anio = forms.IntegerField(initial=2022 , label='Año', min_value=2017, max_value=2022, help_text='admitimos vehículos de hasta 5 años de antigüedad') 
     categoria= forms.CharField(max_length=20)   # tabla
     descripcion= forms.CharField(label='Descripción', max_length=300, widget=forms.Textarea(attrs={'class':'form-control','rows':4}))
     puertas = forms.ChoiceField(choices=PUERTAS,initial='',widget=forms.Select(attrs={'class':'form-control'}))    
