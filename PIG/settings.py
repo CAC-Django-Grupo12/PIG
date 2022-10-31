45a0887e882d89804e28413590cf414e6c77f26b
tresPuntosG <85909026+tresPuntosG@users.noreply.github.com>
2022-10-31 17:57:09 -0300

    Update settings.py

--- PIG/settings.py
+++ PIG/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'PIG',
         'USER': 'postgres',
-        'PASSWORD': 'tresPuntosG',
+        'PASSWORD': 'Grupo12',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
