f50d5f283499b62928d04d10510d7fc6ecd7c59e
tresPuntosG <pablociorciari@gmail.com>
2022-11-01 11:55:18 -0300

    creo modelo Vehiculo

--- productos/models.py
+++ productos/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Vehiculo(models.Model):
+
+    marca= models.CharField(max_length=20)
+    modelo= models.CharField(max_length=30)
+    anio = models.IntegerField()
+    categoria= models.CharField(max_length=20)
+    descripcion= models.CharField(max_length=300)
+    puertas = models.CharField(max_length=1)
+    precio= models.FloatField()
+    fecha_publicacion= models.DateField()
+    # imagen= 
