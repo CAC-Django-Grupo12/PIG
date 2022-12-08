2ab1d600ba91d2d3577ecb4eafc422219a80ad92
tresPuntosG <pablociorciari@gmail.com>
2022-12-08 17:38:31 -0300

    agrego widgets form ingr veh

--- productos/models.py
+++ productos/models.py
@@ -12,13 +12,13 @@ class Categoria(models.Model):
 
 class Vehiculo(models.Model):
 
-    marca= models.CharField(max_length=20)
-    modelo= models.CharField(max_length=30)
-    anio = models.IntegerField()
-    categoria= models.ForeignKey(Categoria, on_delete=models.CASCADE)
-    descripcion= models.CharField(max_length=300)
+    marca= models.CharField(max_length=20, verbose_name='Marca')
+    modelo= models.CharField(max_length=30, verbose_name='Modelo')
+    anio = models.IntegerField(verbose_name='Año')
+    categoria= models.ForeignKey(Categoria, on_delete=models.CASCADE, verbose_name='Categoría')
+    descripcion= models.CharField(max_length=300, verbose_name='Descripción')
     puertas = models.CharField(max_length=1)
     precio= models.FloatField()
-    fecha_publicacion= models.DateField()
+    fecha_publicacion= models.DateField(verbose_name='Fecha publicación')
     seleccionado=models.BooleanField(default=None,null=True)
     imagen=models.ImageField(upload_to='upload/',verbose_name="Imagen:",default=None,null=True)
